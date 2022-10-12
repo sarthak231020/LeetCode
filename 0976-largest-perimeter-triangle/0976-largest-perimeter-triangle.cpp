0b7dbaa691c984fac3948c3b6403177013508b96
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-10-12 12:42:00 +0530

    Time: 103 ms (7.35%), Space: 21.9 MB (71.51%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -5,13 +5,8 @@ public:
         int N = nums.size();
         for(int i=N-1;i>=2;i--) 
         {
-            double s = (nums[i]+nums[i-1]+nums[i-2])/2.0;
-            double area = sqrt(s*(s-nums[i])*(s-nums[i-1])*(s-nums[i-2]));
-            // cout<<s<<" "<<area<<endl;
-            if(area > 0.0)
-            {
-                return nums[i]+nums[i-1]+nums[i-2];   
-            }
+            if(nums[i] < nums[i-1]+nums[i-2]) 
+                return nums[i]+nums[i-1]+nums[i-2];
         }
         return 0;
     }
