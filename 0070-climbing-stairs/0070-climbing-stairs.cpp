3f8eda1cd3612fcf75c3af3edf4bf6a4d30d01ee
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-10-19 11:58:46 +0530

    Time: 4 ms (22.30%), Space: 5.8 MB (83.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -15,15 +15,31 @@ public:
 //     }
     
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
+        // vector<int> dp(n+1,-1);
         // return solve(n,dp); 
         
-        dp[0] = 1;
-        dp[1] = 1;
+        //Iterative Solution :- 
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i=2;i<=n;i++) 
+        // {
+        //     dp[i] = dp[i-1]+dp[i-2];
+        // }
+        // return dp[n];
+        
+        //Space Optimized Solution :- 
+        if(n <= 1) 
+            return 1;
+        int prev2 = 1; // n == 0
+        int prev1 = 1; // n == 1
+        int curr;
         for(int i=2;i<=n;i++) 
         {
-            dp[i] = dp[i-1]+dp[i-2];
+            curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n];
+        
+        return curr;
     }
 };
