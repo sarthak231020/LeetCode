3cccf6aa7e6b284098eba68aad8a056d80d1ec0e
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-10-21 18:13:11 +0530

    Time: 309 ms (54.99%), Space: 210.4 MB (27.49%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -2,36 +2,23 @@ class Solution {
 public:
     bool solve(int ind,int N,vector<int> &stones,int lastJump,vector<vector<int>> &dp) 
     {
-        if(ind >= N) 
-            return false;
-        if(ind == N-1)
+        if(ind == 0)
             return true;
-        // cout<<stones[ind]<<" "<<lastJump<<endl;
-        if(dp[ind][lastJump] != -1) 
-            return dp[ind][lastJump];
-        
         bool flag = false;
-        for(int i=lastJump-1;i<=lastJump+1;i++) 
+        for(int j=0;j<ind;j++) 
         {
-            int k = ind+1;
-            while(k < N) 
-            {
-                if(stones[ind]+i == stones[k]) 
-                {
-                    if(solve(k,N,stones,i,dp))
-                    {
-                        flag = true;
-                        return dp[ind][lastJump] = true;
-                    }
-                }
-                k++;
-            }
-            if(flag == true)
-            {
-                break;
-            }
+            int dist = stones[ind]-stones[j];
+            if(dist < 0 || dist > N || !dp[j][dist]) continue;
+
+            if(dist-1 >= 0)
+                dp[ind][dist-1] = true;
+            if(dist+1 <= N) 
+                dp[ind][dist+1] = true;
+            dp[ind][dist] = true;
+            flag = true;
+            if(solve(ind-1,N,stones,dist,dp)) 
+                return dp[ind][lastJump] = true;
         }
-        
         return dp[ind][lastJump] = flag;
     }
     
@@ -39,9 +26,31 @@ public:
         if(stones[1] != 1) 
             return false;
         int N = stones.size();
-        vector<vector<int>> dp(N+1,vector<int>(N+1,-1));
+        vector<vector<int>> dp(N+1,vector<int>(N+1,0));
+        // return solve(1,stones.size(),stones,1,dp);
+        dp[0][1] = true;
+        // return solve(N-1,N,stones,N-1,dp);
+// //         dp[0][1] = true;
         
+// //         for()
+        // dp[]
+        for(int i=1;i<N;i++) 
+        {
+            for(int j=0;j<i;j++) 
+            {
+                int dist = stones[i]-stones[j]; 
+                if(dist < 0 || dist > N || !dp[j][dist]) 
+                    continue; 
+                dp[i][dist] = 1; 
+                if(dist-1 >= 0) 
+                    dp[i][dist-1] = 1;
+                if(dist+1 <= N) 
+                    dp[i][dist+1] = 1;
+                if(i == N-1) 
+                    return true;
+            }
+        }
+        return false;
         
-        return solve(1,stones.size(),stones,1,dp);
     }
 };
