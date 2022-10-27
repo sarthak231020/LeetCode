2d39f88a27267327d2ab32634e8ade21a389531e
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-10-27 20:12:57 +0530

    Time: 264 ms (86.51%), Space: 113.9 MB (46.54%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -8,13 +8,17 @@ public:
         for(int i=0;i<N;i++) 
         {
             sum += nums[i];
-            
-            if(mp.find(sum%K) != mp.end() && i-mp[sum%K] >= 2) //That remainder already exist means the sum from that index till here is multiple of K that's why we are getting the same remainder again & their index difference is more than two elements in subarray.
+        
+            int remainder = sum%K;
+            if(mp.find(remainder) == mp.end()) //It means we seeing it for the first time.
             {
+                mp[remainder] = i;
+            }
+            else if(i-mp[remainder] > 1) 
+            {
+                //That remainder already exist means the sum from that index till here is multiple of K that's why we are getting the same remainder again & their index difference is more than two elements in subarray.
                 return true;
             }
-            if(mp.find(sum%K) == mp.end())
-                mp[sum%K] = i;
         }
         
         return false;
