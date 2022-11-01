b1286dfc1c3ef71b4173e6c6fe44c07b7c71e3a2
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-01 20:06:28 +0530

    Time: 69 ms (14.96%), Space: 14.5 MB (5.40%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        queue<pair<int,pair<int,int>>> q; //Ball No, Current Position of Ball (x,y).
+        int M = grid.size(); 
+        int N = grid[0].size();
+        
+        for(int i=0;i<N;i++) 
+        {
+            q.push({i,{0,i}});
+        }
+        
+        vector<int> ans(N,-1);
+        while(!q.empty()) 
+        {
+            auto t = q.front();
+            q.pop();
+            int B = t.first;
+            int x = t.second.first;
+            int y = t.second.second;
+            
+            if(x == M)
+            {
+                ans[B] = y; //Ball no B gets out of yth column.
+            }
+            else 
+            {
+                int J = grid[x][y]+y;
+                
+                if((J>=0 && J<N) && (grid[x][y] == grid[x][J])) 
+                    q.push({B,{x+1,J}});
+            }
+            
+        }
+        
+        return ans;
+    }
+};
