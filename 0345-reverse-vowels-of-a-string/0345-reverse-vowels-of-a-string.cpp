4843136ed85c3a171c1e8f56757c5c1d0322dc61
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-04 12:55:38 +0530

    Time: 20 ms (32.93%), Space: 7.7 MB (61.66%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -9,25 +9,48 @@ public:
         
     }
     string reverseVowels(string s) {
-        vector<int> v;
+//         vector<int> v;
         
-        for(int i=0;i<s.size();i++) 
-        {
-            if(isVowel(s[i])) 
-                v.push_back(i);
-        }
+        // for(int i=0;i<s.size();i++) 
+        // {
+        //     if(isVowel(s[i])) 
+        //         v.push_back(i);
+        // }
         // for(auto i:v) 
         // {
         //     cout<<i<<" ";
         // }
         // cout<<endl;
-        int i=0,j=v.size()-1; 
-        while(i <= j) 
+//         int i=0,j=v.size()-1; 
+//         while(i <= j) 
+//         {
+//             swap(s[v[i]],s[v[j]]); 
+//             // cout<<s<<endl;
+//             i++;
+//             j--;
+//         }
+        
+//         return s;
+        
+        int i=0,j = s.size()-1;
+        while(i<=j) 
         {
-            swap(s[v[i]],s[v[j]]); 
-            // cout<<s<<endl;
-            i++;
-            j--;
+            // cout<<s<<" "<<s[i]<<" "<<s[j]<<endl;
+            if(isVowel(s[i]) && isVowel(s[j])) 
+            {   
+                // cout<<"IF"<<endl;
+                swap(s[i],s[j]);
+                i++; 
+                j--;
+            }
+            else 
+            {
+                // cout<<"ELSE "<<s[i]<<" "<<s[j]<<endl;
+                if(isVowel(s[i]) == false) //s[i] is consonant.
+                    i++;
+                if(isVowel(s[j]) == false) //s[j] is consonant.
+                    j--;
+            }
         }
         
         return s;
