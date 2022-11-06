2c4ffd7071845aa5182d188e30efc24948d481b2
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-07 00:00:25 +0530

    Time: 12 ms (28.74%), Space: 10.8 MB (32.18%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.cpp
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -4,7 +4,7 @@ public:
         if(k == 1) 
         {
             string ans = s;
-            for(int i=0;i<s.size();i++) 
+            for(int i=1;i<s.size();i++) 
             {
                 string temp = s.substr(i)+s.substr(0,i);
                 if(temp < ans) 
@@ -15,7 +15,7 @@ public:
             return ans;
         }       
         else
-        {
+        {//In case of k >= 2 we are able to generate every permutation of string so we are able to generate the lexicographically smallest string 
             sort(s.begin(),s.end()); 
             return s;
         }
