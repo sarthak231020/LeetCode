3c3fe601031d05e3644c8285e0e4e13bf8895fbd
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-16 19:12:05 +0530

    Time: 102 ms (25.21%), Space: 18.4 MB (54.24%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -21,11 +21,54 @@ public:
         
         return dp[ind][amount] = (pick+notPick);
     }
+    int solveTabulated(int amount, vector<int>& coins)
+    {
+        // cout<<"NEXT"<<endl;
+        int n = coins.size();
+        vector<vector<int>> dp(n,vector<int> (amount+1,0));
+        
+        //for all amount as 0 we should fill 1 since we are able to generate it no matter what.
+        for(int i=0;i<n;i++) 
+        {
+            dp[i][0] = 1;
+        }
+        for(int amt = 1; amt <= amount; amt++) 
+        {
+            if(amt%coins[0] == 0) 
+                dp[0][amt] = 1;
+            else
+                dp[0][amt] = 0;
+        }
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int amt = 1;amt<=amount;amt++) 
+            {
+                int pick = 0;
+                if(coins[ind] <= amt)
+                {
+                    pick = dp[ind][amt-coins[ind]];
+                }
+                int notPick = dp[ind-1][amt];
+
+                dp[ind][amt] = pick+notPick;
+                // cout<<ind<<" "<<amt<<" "<<min(pick,notPick)<<endl; 
+            }
+        }
+        
+        return dp[n-1][amount];
+        
+    }
+    
     int change(int amount, vector<int>& coins) {
+        /* FOR MEMOIZED CODE :- TC :- O(N) since
         int n = coins.size();
         vector<vector<int>> dp(n,vector<int> (amount+1,-1));
-        
         return solve(n-1,amount,coins,dp);
+        */
+        
+        //Tabulated Code:-
+        
+        return solveTabulated(amount,coins);
         
     }
 };
