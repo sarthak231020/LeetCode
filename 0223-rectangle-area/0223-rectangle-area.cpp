d304ff7f5e00bb57055c079c8f56d4923c572bf5
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-17 18:17:01 +0530

    Time: 8 ms (86.37%), Space: 6 MB (32.00%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    int calc(int p1,int p2) 
-    {
-        return p2-p1;
-    }
     
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        cout<<"NEXT"<<endl;
+        //TC :- O(1).SC :- O(1)
         int ox1 = max(ax1,bx1);
         int oy1 = max(ay1,by1);
         int ox2 = min(ax2,bx2);
         int oy2 = min(ay2,by2);
         
-        int l1 = calc(ax1,ax2);
-        int h1 = calc(ay1,ay2);
-        int l2 = calc(bx1,bx2);
-        int h2 = calc(by1,by2);
-        cout<<l1<<" "<<l2<<" "<<h1<<" "<<h2<<endl;
+        int l1 = ax2-ax1;
+        int h1 = ay2-ay1;
+        int l2 = bx2-bx1;
+        int h2 = by2-by1;
+        
         int a1 = l1*h1;
         int a2 = l2*h2;
         
-        // if((ax1 <= bx1 && bx1 <= ax2 && ay1 <= by1 && by1 <= ay2) || (ax1 <= bx2 && bx2 <= ax2 && ay1 <= by2 && by2 <= ay2) || (bx1 <= ax1 && ax1 <= bx2 && by1 <= ay1 && ay1 <= by2) || (bx1 <= ax2 && ax2 <= bx2 && by1 <= ay2 && ay2 <= by2))
-        // {
-            int ol1 = calc(ox1,ox2);
-            int oh1 = calc(oy1,oy2);
-            if(ol1 >= 0 && oh1 >= 0) 
-            {
+        int ol1 = ox2-ox1;
+        int oh1 = oy2-oy1;
+        if(ol1 >= 0 && oh1 >= 0)  //means overlapping portion is not fake so minus the overlapping area.
+        {
             int oa = ol1*oh1;
             if(oa >= 0) 
                 return (a1+a2-oa);
-            }
-        // }
+        }
+        //in case it is not overlapping ol1 or oh1 will come as negative. so we will just return the actual are 
         return (a1+a2);
-            
     }
 };
