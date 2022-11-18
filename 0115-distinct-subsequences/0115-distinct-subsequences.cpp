ba82f2535cf89864617096cb06444d83486e270f
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-18 20:35:47 +0530

    Time: 38 ms (86.47%), Space: 6.4 MB (95.79%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -16,9 +16,75 @@ public:
             return dp[ind1][ind2] = solve(ind1-1,ind2,s1,s2,dp)%MOD;
     }
     
+    int solveTabulated(string &s1,string &s2) 
+{
+    int n1 = s1.size(),n2 = s2.size();
+    vector<vector<int>> dp(n1+1,vector<int> (n2+1,0));
+    for(int ind1=0;ind1<=n1;ind1++) 
+    {
+        for(int ind2=0;ind2<=n2;ind2++) 
+        {
+            if(ind2 == 0) 
+                dp[ind1][ind2] = 1;
+            else if(ind1 == 0) 
+                dp[ind1][ind2] = 0;
+            else
+            {
+                if(s1[ind1-1] == s2[ind2-1]) 
+                    dp[ind1][ind2] = (dp[ind1-1][ind2-1]%MOD + dp[ind1-1][ind2]%MOD)%MOD;
+                else
+                    dp[ind1][ind2] = dp[ind1-1][ind2]%MOD;
+            }
+        }
+    }
+    
+    return dp[n1][n2];
+}
+
+int solveTabulatedSpaceOpt(string &s1,string &s2) 
+{
+    int n1 = s1.size(),n2 = s2.size();
+    vector<int> prev(n2+1,0),curr(n2+1,0);
+    
+    prev[0] = 1;
+    
+    for(int ind1=0;ind1<=n1;ind1++) 
+    {
+        for(int ind2=0;ind2<=n2;ind2++) 
+        {
+            if(ind2 == 0) 
+                curr[ind2] = 1;
+            else if(ind1 == 0) 
+                curr[ind2] = 0;
+            else
+            {
+                if(s1[ind1-1] == s2[ind2-1]) 
+                    curr[ind2] = (prev[ind2-1]%MOD + prev[ind2]%MOD)%MOD;
+                else
+                    curr[ind2] = prev[ind2]%MOD;
+            }
+        }
+        prev = curr;
+    }
+    
+    return prev[n2];
+}
+
+
+    
     int numDistinct(string s, string t) {
-        int n1 = s.size(),n2 = t.size();
+        
+        /* memoized code :- TC-> O(n*m) SC -> O(n*m)+O(stack Space -> O(n+m));
+        int n1 = s.size(),n2=t.size();
         vector<vector<int>> dp(n1+1,vector<int> (n2+1,-1));
         return solve(n1,n2,s,t,dp);
+
+        */
+
+        /*Tabulated Code :- TC -> O(n*m) SC-> O(n*m)*/
+        //return solveTabulated(s,t);
+
+        /*Space Optimized TC -> O(n*m) SC-> O(2*m)*/
+        return solveTabulatedSpaceOpt(s,t);
     }
 };
