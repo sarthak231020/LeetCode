a6078127035dd4b3caa3d1c3ad37e5687092fedc
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-22 11:56:55 +0530

    Time: 1117 ms (8.94%), Space: 11.9 MB (20.13%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -28,6 +28,40 @@ public:
         return dp[i][n] = min(pick,notPick);
     }
     
+    int solveSpaceOpt(vector<int> &perSq,int n)
+    {
+        vector<int> prev(n+1,0),curr(n+1,0);
+        
+        for(int j=1;j<=n;j++) 
+        {
+            if(n%perSq[0] == 0) 
+            {
+                prev[j] = n/perSq[0];
+            }   
+            else 
+            {
+                prev[j] = 1e9;
+            }
+        }
+        prev[0] = 0; 
+        
+        for(int i=1;i<=perSq.size();i++) 
+        {
+            curr[0] = 0; // when n is zero the result is 0.
+            for(int j=1;j<=n;j++) 
+            {
+                int pick = 1e9; 
+                if(j-perSq[i-1] >= 0)
+                    pick = 1+curr[j-perSq[i-1]];
+                int notPick = prev[j];
+
+                curr[j] = min(pick,notPick);
+            }
+            prev = curr;
+        }
+        return prev[n];
+    }
+    
     int numSquares(int n) {
         vector<int> perSq; // an array which will have perfect squares stored. 
         
@@ -36,9 +70,11 @@ public:
             perSq.push_back(i*i);
         }
         
-        vector<vector<int>> dp(perSq.size(),vector<int> (n+1,-1));
+        // FOR MEMOIZED CODE 
+//         vector<vector<int>> dp(perSq.size(),vector<int> (n+1,-1));
+//         return solve(perSq.size()-1,n,perSq,dp);
         
-        return solve(perSq.size()-1,n,perSq,dp);
+        return solveSpaceOpt(perSq,n);
         
 //         int ans = 0;
 //         for(int i=99;i>=0;i--) 
