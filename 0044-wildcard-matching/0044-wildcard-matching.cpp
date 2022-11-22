68f0bf0998d9f60810d517a36907d6c37845b299
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-22 13:04:03 +0530

    Time: 165 ms (26.08%), Space: 27.9 MB (13.52%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -35,10 +35,67 @@ public:
         return dp[i][j] = false;
     }
     
+    bool solveTabulated(string &s,string &p)
+    {
+        int n1 = s.size(); 
+        int n2 = p.size();
+        vector<vector<int>> dp(n1+1,vector<int> (n2+1,0));
+        
+        dp[0][0] = 1;
+        
+        for(int i=1;i<=n1;i++) //When j is 0 return false
+        {
+            dp[i][0] = 0;
+        }
+        
+        for(int j=1;j<=n2;j++) 
+        {
+            bool flag = true;
+            int k = j;
+            while(k-1 >= 0)
+            {
+                if(p[k-1] != '*')
+                {
+                    flag = false;
+                    break; 
+                }
+                k--;
+            }
+            dp[0][j] = flag; 
+        }
+        
+        
+        for(int i=1;i<=n1;i++) 
+        {
+            for(int j=1;j<=n2;j++) 
+            {
+                if(s[i-1] == p[j-1] || p[j-1] == '?') 
+                {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[j-1] == '*')
+                {
+                    dp[i][j] = (dp[i-1][j] | dp[i][j-1]);
+                }
+                else
+                {   
+                    //Means a mismatch so we can not overcome a mismatch.
+                    dp[i][j] = false;
+                }
+            }
+        }
+        
+        return dp[n1][n2];
+    }
+    
     bool isMatch(string s, string p) {
         int n1 = s.size(),n2 = p.size();
-        vector<vector<int>> dp(n1+1,vector<int> (n2+1,-1));
-        return solve(n1,n2,s,p,dp);
+        
+        //Memoized TC :- O(M*N) SC:- O(M*N)(DP ARRAY)+O(M+N)(ASS).
+//         vector<vector<int>> dp(n1+1,vector<int> (n2+1,-1));
+//         return solve(n1,n2,s,p,dp);
+        
+        return solveTabulated(s,p);
         
         
     }
