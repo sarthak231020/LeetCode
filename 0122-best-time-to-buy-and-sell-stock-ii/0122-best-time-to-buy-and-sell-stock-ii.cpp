8552e71194cdea3f3dff32db997e6a163fbd1241
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-22 19:51:06 +0530

    Time: 32 ms (10.20%), Space: 17.4 MB (6.92%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    
+    int solveGreedy(vector<int> &prices)
+    {
         int profit = 0;
         
         for(int i=1;i<prices.size();i++) 
@@ -11,5 +13,44 @@ public:
             }
         }
         return profit;
+    }
+                // index  buy=1(means you are allowed to buy) and (buy = 0 means you are not allowed to buy).
+    int solveDp(int i,int buy,vector<int> &prices,vector<vector<int>> &dp) 
+    {
+        if(i == prices.size()) 
+        {
+            return 0; // no matter what you are holding you can't sell it now 
+        }
+        
+        if(dp[i][buy] != -1) 
+            return dp[i][buy];
+        
+        int profit = 0;
+        if(buy) // means if we are allowed to buy then it's our choice to buy or not.
+        {
+            profit = max(-prices[i]+solveDp(i+1,0,prices,dp),0+solveDp(i+1,1,prices,dp));
+            // first case we are buying it using it's price so -prices[i] and going forward to not buy it again
+            //Second Case we are not buying and can buy other stocks(passing buy = 1).
+            
+        }
+        else
+        {
+            //again we have two choices either we are selling it now pr may be we will sell it later.
+            //First case we allow to buy again 
+            //Second Case we won't allow to buy 
+            profit = max(prices[i]+solveDp(i+1,1,prices,dp),0+solveDp(i+1,0,prices,dp));
+            
+        }
+        
+        return dp[i][buy] = profit;
+    }
+    
+    
+    int maxProfit(vector<int>& prices) {
+        
+        //Greedy Solution:- TC:- O(N) SC:- O(1).
+                                                //    buy have two states 0 and 1 only
+        vector<vector<int>> dp(prices.size(),vector<int> (2,-1));
+        return solveDp(0,1,prices,dp);
     }
 };
