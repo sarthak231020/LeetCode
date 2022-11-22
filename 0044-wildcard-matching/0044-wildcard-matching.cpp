6693c8913326606436f0e8d773db0efff42f4558
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-22 13:07:49 +0530

    Time: 87 ms (66.47%), Space: 7.1 MB (83.95%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -88,6 +88,62 @@ public:
         return dp[n1][n2];
     }
     
+    bool solveTabulatedSpaceOpt(string &s,string &p)
+    {
+        int n1 = s.size(); 
+        int n2 = p.size();
+        vector<int> prev(n2+1,0),curr(n2+1,0);
+        
+        prev[0] = 1;
+        
+        // for(int i=1;i<=n1;i++) //When j is 0 return false
+        // {
+        //     dp[i][0] = 0;
+        // }
+        
+        for(int j=1;j<=n2;j++) 
+        {
+            bool flag = true;
+            int k = j;
+            while(k-1 >= 0)
+            {
+                if(p[k-1] != '*')
+                {
+                    flag = false;
+                    break; 
+                }
+                k--;
+            }
+            prev[j] = flag; 
+        }
+        
+        
+        for(int i=1;i<=n1;i++) 
+        {
+            curr[0] = 0; //when j is 0 then return false;
+            for(int j=1;j<=n2;j++) 
+            {
+                if(s[i-1] == p[j-1] || p[j-1] == '?') 
+                {
+                    curr[j] = prev[j-1];
+                }
+                else if(p[j-1] == '*')
+                {
+                    curr[j] = (prev[j] | curr[j-1]);
+                }
+                else
+                {   
+                    //Means a mismatch so we can not overcome a mismatch.
+                    curr[j] = false;
+                }
+            }
+            prev = curr;
+        }
+        
+        return prev[n2];
+    }
+    
+    
     bool isMatch(string s, string p) {
         int n1 = s.size(),n2 = p.size();
         
@@ -95,8 +151,11 @@ public:
 //         vector<vector<int>> dp(n1+1,vector<int> (n2+1,-1));
 //         return solve(n1,n2,s,p,dp);
         
-        return solveTabulated(s,p);
+        //Tabulated TC :- O(M*N) SC:- O(M*N)(DP ARRAY).
+        // return solveTabulated(s,p);
         
+        //Space Opt TC :- O(M*N) SC:- O(2*N).
+        return solveTabulatedSpaceOpt(s,p);
         
     }
 };
