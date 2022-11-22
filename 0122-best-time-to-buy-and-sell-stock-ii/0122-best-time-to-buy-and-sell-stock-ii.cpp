e87cec77d5bd28eb1c8baa1f28f3d615098c01c0
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-22 20:16:01 +0530

    Time: 16 ms (45.69%), Space: 13 MB (59.11%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -45,12 +45,98 @@ public:
         return dp[i][buy] = profit;
     }
     
+    int solveDpTabulated(vector<int> &prices) 
+    {
+        int n = prices.size();
+        
+        vector<vector<int>> dp(n+1,vector<int> (2,0));
+        
+        //Converting the base case.
+        dp[n][0] = 0;
+        dp[n][1] = 0; 
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy = 0;buy<=1;buy++)
+            {
+                int profit = 0;
+                if(buy) // means if we are allowed to buy then it's our choice to buy or not.
+                {
+                    profit = max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+                    // first case we are buying it using it's price so -prices[i] and going forward to not buy it again
+                    //Second Case we are not buying and can buy other stocks(passing buy = 1).
+
+                }
+                else
+                {
+                    //again we have two choices either we are selling it now pr may be we will sell it later.
+                    //First case we allow to buy again 
+                    //Second Case we won't allow to buy 
+                    profit = max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
+
+                }
+
+                dp[i][buy] = profit;
+            }   
+        }
+        
+        return dp[0][1]; // as we know we are passing 1 initially.
+        
+    }
+    
+    int solveDpTabulatedSpaceOpt(vector<int> &prices) 
+    {
+        int n = prices.size();
+        
+        vector<int> nex(2,0),curr(2,0);
+        
+        //Converting the base case.
+        nex[0] = 0;
+        nex[1] = 0; 
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy = 0;buy<=1;buy++)
+            {
+                int profit = 0;
+                if(buy) // means if we are allowed to buy then it's our choice to buy or not.
+                {
+                    profit = max(-prices[i]+nex[0],0+nex[1]);
+                    // first case we are buying it using it's price so -prices[i] and going forward to not buy it again
+                    //Second Case we are not buying and can buy other stocks(passing buy = 1).
+
+                }
+                else
+                {
+                    //again we have two choices either we are selling it now pr may be we will sell it later.
+                    //First case we allow to buy again 
+                    //Second Case we won't allow to buy 
+                    profit = max(prices[i]+nex[1],0+nex[0]);
+
+                }
+
+                curr[buy] = profit;
+            }   
+            nex = curr;
+        }
+        
+        return nex[1]; // as we know we are passing 1 initially.       
+    }
+    
     
     int maxProfit(vector<int>& prices) {
         
         //Greedy Solution:- TC:- O(N) SC:- O(1).
                                                 //    buy have two states 0 and 1 only
-        vector<vector<int>> dp(prices.size(),vector<int> (2,-1));
-        return solveDp(0,1,prices,dp);
+        //Memoized Solution:- TC:- O(N*2) SC:- O(N*2)(DP ARRAY)+O(N) (Aux Stack Space Since we hit the base case after the N rec call)
+        // vector<vector<int>> dp(prices.size(),vector<int> (2,-1));
+        // return solveDp(0,1,prices,dp);
+        
+        //Tabulated Solution :- TC:- O(N*2). SC:- O(N*2).
+        // return solveDpTabulated(prices);
+        
+        //Space Optimized Solution :- TC :- O(N*2) SC:- O(1). 
+        return solveDpTabulatedSpaceOpt(prices);
+        
     }
 };
