ef8c8301ea5cc1524736d67d6b15de6dbcd6add6
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-11-23 12:44:49 +0530

    Time: 520 ms (52.17%), Space: 75.3 MB (78.03%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -141,6 +141,109 @@ public:
         return next[1][2];
     }
     
+    int solve2dDp(int i,int tran,vector<int> &prices,vector<vector<int>> &dp) 
+    {
+        if(i == prices.size() || tran == 0) 
+        {
+            return 0;
+        }
+        
+        if(dp[i][tran] != -1) 
+            return dp[i][tran];
+        
+        int profit = 0; 
+        //tran is even (starting at 4) whenever we are allowed to buy.
+        if(tran%2 == 0) //allowed to buy.
+        {
+            //1st case -> buy is completed so tran-1 so we are not allowed buy again until we sell that stock.
+            //2nd case -> we have not buy that stock so we are allowed to buy next coming stocks.
+            profit = max(-prices[i]+solve2dDp(i+1,tran-1,prices,dp)
+                         ,0+solve2dDp(i+1,tran,prices,dp));
+        }
+        else
+        {
+            //1st case -> sell is completed so from next stock we are allowed to buy.
+            //2nd case -> we are not selling the stock now we are not allowed to buy any other stock.
+            profit = max(prices[i]+solve2dDp(i+1,tran-1,prices,dp)
+                         ,0+solve2dDp(i+1,tran,prices,dp));
+        }
+        
+        return dp[i][tran] = profit;
+        
+    }
+    
+    int solve2dDpTabulated(vector<int> &prices) 
+    {
+        
+        int n = prices.size();
+        vector<vector<int>> dp(n+1,vector<int> (5,0));
+        
+        //No need to fill up the base case since we have already filled up dp with zeros.
+        for(int i=n-1;i>=0;i--) 
+        {
+            for(int tran=1;tran<=4;tran++) 
+            {
+                int profit = 0; 
+                //tran is even (starting at 4) whenever we are allowed to buy.
+                if(tran%2 == 0) //allowed to buy.
+                {
+                    //1st case -> buy is completed so tran-1 so we are not allowed buy again until we sell that stock.
+                    //2nd case -> we have not buy that stock so we are allowed to buy next coming stocks.
+                    profit = max(-prices[i]+dp[i+1][tran-1]
+                                 ,0+dp[i+1][tran]);
+                }
+                else
+                {
+                    //1st case -> sell is completed so from next stock we are allowed to buy.
+                    //2nd case -> we are not selling the stock now we are not allowed to buy any other stock.
+                    profit = max(prices[i]+dp[i+1][tran-1]
+                                 ,0+dp[i+1][tran]);
+                }
+
+                dp[i][tran] = profit;   
+            }
+        }
+        
+        return dp[0][4];
+        
+    }
+    
+    int solve2dDpSpaceOpt(vector<int> &prices) 
+    {
+        
+        int n = prices.size();
+        vector<int> next(5,0),curr(5,0);
+        
+        //No need to fill up the base case since we have already filled up dp with zeros.
+        for(int i=n-1;i>=0;i--) 
+        {
+            for(int tran=1;tran<=4;tran++) 
+            {
+                int profit = 0; 
+                //tran is even (starting at 4) whenever we are allowed to buy.
+                if(tran%2 == 0) //allowed to buy.
+                {
+                    //1st case -> buy is completed so tran-1 so we are not allowed buy again until we sell that stock.
+                    //2nd case -> we have not buy that stock so we are allowed to buy next coming stocks.
+                    profit = max(-prices[i]+next[tran-1]
+                                 ,0+next[tran]);
+                }
+                else
+                {
+                    //1st case -> sell is completed so from next stock we are allowed to buy.
+                    //2nd case -> we are not selling the stock now we are not allowed to buy any other stock.
+                    profit = max(prices[i]+next[tran-1]
+                                 ,0+next[tran]);
+                }
+
+                curr[tran] = profit;   
+            }
+            next = curr;
+        }
+        
+        return next[4];
+        
+    }
     
     int maxProfit(vector<int>& prices) {
         
@@ -148,9 +251,24 @@ public:
         // vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>> (2,vector<int>(3,-1)));
         // return solve(0,1,2,prices,dp);
         
+        //Tabulated :-  TC :- O(N*2*3) SC :- O(N*2*3)
         // return solveTabulated(prices);
         
-        return solveTabulatedSpaceOpt(prices);
+        //Space Optimized :-  TC :- O(N*2*3) SC:- O(2*2*3) -> O(1).
+        // return solveTabulatedSpaceOpt(prices);
+        
+        //Using 2d DP.
+        //Memoized TC:- O(N*5). SC:- O(N*5)(DP ARRAY)+O(N)(ASS).
+        // vector<vector<int>> dp(prices.size(),vector<int> (5,-1));
+        // return solve2dDp(0,4,prices,dp);
+        
+        //Using 2d DP 
+        //Tabulated TC:- O(N*5) SC :- O(N*5)
+        // return solve2dDpTabulated(prices);
+        
+        //Using 2d DP 
+        //Space Optimized :- TC:- O(N*5) SC:- O(2*5) //O(1).
+        return solve2dDpSpaceOpt(prices);
         
     }
 };
