595988a70f227fb49e0ac5befa461f811a18720f
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2022-12-02 17:22:59 +0530

    Time: 139 ms (46.70%), Space: 10.3 MB (41.88%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -16,25 +16,51 @@ public:
             int steps = (arr[j+1]-arr[i-1])
                         + solve(i,k-1,arr,dp)
                         + solve(k+1,j,arr,dp);
-    //                                       A     B     C
-            //for example if we multiplying 10*20 20*30 30*40 
-            //                                 i  i         j
-            // so we want A*B + B*C
-            // 10 * 20 * 40 is required to added for final multiplication answer.
-            // for matrices of size M*N N*O the number of multiplications req are 
-            // -> M*N*O.
             mini = min(mini,steps);
         }
 
         return dp[i][j] = mini;
     }
+    
+    
+    int solveTabulated(vector<int> &arr,int N) 
+    {
+        vector<vector<int>> dp(N+1,vector<int> (N+1,0));
+        for(int i=N-1;i>=1;i--) 
+        {
+            for(int j=i;j<N-1;j++)
+            {
+                int mini = INT_MAX;
+                for(int k = i;k<=j;k++)
+                {
+                    int steps = (arr[j+1]-arr[i-1])
+                                + dp[i][k-1]
+                                + dp[k+1][j];
+                    mini = min(mini,steps);
+                }
+
+                dp[i][j] = mini;
+
+            }
+        }
+        
+        return dp[1][N-2];
+    }
+    
+    
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(0);
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
-        
         int k = cuts.size();
-        vector<vector<int>> dp(k,vector<int> (k,-1));
-        return (int)solve(1,k-2,cuts,dp);
+        
+        //Memoized Code :- TC-> O(N^3) SC-> O(N^2)(DP ARRAY)+O(N)(ASS).
+//         vector<vector<int>> dp(k,vector<int> (k,-1));
+//         return (int)solve(1,k-2,cuts,dp);
+        
+        //Tabulated Code :- TC-> O(N^3) SC->O(N^2)(DP ARRAY).
+        return solveTabulated(cuts,k);
+        
+        
     }
 };
