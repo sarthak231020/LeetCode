d51a2669845ddb49e64311646fe8476ca25d3ab6
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-01-04 18:29:02 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (74.64%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,10 +1,41 @@
 class Solution {
 public:
+    
+    bool allCapitals(string &word) 
+    {
+        for(auto i:word) 
+        {
+            if(islower(i)) 
+                return false; 
+        }
+        return true; 
+    }
+    
+    bool allSmalls(string &word) 
+    {
+        for(auto i:word)
+        {
+            if(isupper(i))
+                return false;
+        }
+        return true;
+    }
+    
+    bool firstCapAndAllSmall(string &word) 
+    {
+        if(islower(word[0])) 
+            return false;
+        for(int i=1;i<word.size();i++) 
+        {
+            if(isupper(word[i]))
+                return false;
+        }
+        return true;
+    }
+    
+    
     bool detectCapitalUse(string word) {
-    for(int i = 1; i < word.size(); i++){
-        if(isupper(word[1]) != isupper(word[i]) || 
-           islower(word[0]) && isupper(word[i])) return false;
-    }        
-    return true;
-}
+        return (allCapitals(word) || allSmalls(word) || firstCapAndAllSmall(word));
+        
+    }
 };
