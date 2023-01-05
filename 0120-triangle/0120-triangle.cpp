a7a0e5eb415e5c65dd381fa25c827a8df28c7dec
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-01-05 20:18:13 +0530

    Time: 25 ms (5.53%), Space: 9.1 MB (34.31%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -9,12 +9,29 @@ public:
         
         
         return dp[i][j] = triangle[i][j] + min(solve(i+1,j,triangle,dp),solve(i+1,j+1,triangle,dp)); 
-        
-        
     }
+    
+    int solveTabulated(vector<vector<int>> &triangle)
+    {
+        int n = triangle.size();
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0)); 
+        //No need to write base case since we already filled the dp with zeros.
+        
+        for(int i=n-1;i>=0;i--) 
+        {
+            for(int j=i;j>=0;j--) 
+            {
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
+    
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size(); 
-        vector<vector<int>> dp(n,vector<int> (n,-1)); 
-        return solve(0,0,triangle,dp);
+        // int n = triangle.size(); 
+        // vector<vector<int>> dp(n,vector<int> (n,-1)); 
+        // return solve(0,0,triangle,dp);
+        
+        return solveTabulated(triangle);
     }
 };
