f5981292e1055dfdd3e33da96e3be3129473b142
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-01-05 14:51:41 +0530

    Time: 540 ms (64.85%), Space: 89.8 MB (28.76%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,15 +1,35 @@
-bool cmp(vector<int>& a, vector<int>& b) {return a[1] < b[1];}
 class Solution {
-public:  
-    int findMinArrowShots(vector<vector<int>>& segments) {
-        sort(segments.begin(), segments.end(), cmp);
-        int ans = 0, arrow = 0;
-        for (int i = 0; i < segments.size(); i ++) {
-            if (ans == 0 || segments[i][0] > arrow) {
-                ans ++;
-                arrow = segments[i][1];
+public:
+    static bool comp(vector<int> &v1,vector<int> &v2) 
+    {
+        if(v1[1] < v2[1])
+            return true; 
+        if(v1[1] == v2[1]) 
+            return v1[0] <= v2[0];
+        return false;
+    }
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(),points.end(),comp);
+        int shot = points[0][1];
+        int ans = 1;
+        // cout<<"NEXT"; 
+        // for(auto i:points) 
+        // {
+        //     for(auto j:i)
+        //         cout<<j<<" ";
+        //     cout<<endl;
+        // }
+        
+        for(int i=1;i<points.size();i++) 
+        {
+            if(shot < points[i][0])
+            {
+                ans++;
+                shot = points[i][1];
             }
         }
+        
         return ans;
+        
     }
 };
