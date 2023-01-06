8570bbbcfd18ffd8923f2ffbf32f505056a37787
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-01-06 20:49:45 +0530

    Time: 458 ms (39.12%), Space: 80 MB (39.71%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,30 +1,60 @@
 class Solution {
 public:
-    bool solve(int i,int k,vector<int> &arr,vector<vector<int>> &dp) 
-    {
-        if(k == 0) 
-            return true; 
-        if(i < 0 || k < 0)  
-            return false;
-
-        if(dp[i][k] != -1) 
-            return dp[i][k]; 
-        bool pick =solve(i-1,k-arr[i],arr,dp);
-        if(pick == true) 
-            return true;
-        bool notPick = solve(i-1,k,arr,dp);
-        if(notPick == true) 
-            return true;
-
-        return dp[i][k] = (pick || notPick);
-    }
+    //Recursive Code :- 
+//     bool solve(int i,int sum,vector<int> &nums,vector<vector<int>> &dp) 
+//     {
+//         if(sum == 0) 
+//             return dp[i][sum] = true; 
+//         if(i == nums.size()) 
+//             return false;
+//         if(dp[i][sum] != -1) 
+//             return dp[i][sum];
+        
+//         if(nums[i] > sum) 
+//             return dp[i][sum] = solve(i+1,sum,nums,dp);
+//         else 
+//             return dp[i][sum] = solve(i+1,sum-nums[i],nums,dp) || solve(i+1,sum,nums,dp);
+//         return false; //just to avoid compilation err.
+//     }
+    
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        for(auto num:nums) 
-            sum += num; 
-        if(sum%2 == 1)  //Since we can not factorize if sum is odd.
+        for(int i=0;i<nums.size();i++) 
+        {
+            sum += nums[i];
+        }
+        
+//         vector<vector<int>> dp(200,vector<int> (20000+1,-1));
+        if(sum%2 == 1) //if sum is odd then it is not possible to partition array 
             return false;
-        vector<vector<int>> dp(nums.size(),vector<int> (sum/2 + 1 , -1));
-        return solve(nums.size()-1,sum/2,nums,dp);
+        
+//         return solve(0,sum/2,nums,dp);
+        
+        //Iterative Code :- Top Down DP 
+        vector<vector<bool>> dp(200+1,vector<bool> (20000+1,-1));
+        int N = nums.size();
+        for(int i=0;i<N;i++) 
+        {
+            dp[i][0] = 1;
+        }
+        for(int j=1;j<=sum/2;j++) 
+        {
+            dp[0][j] = 0;
+        }
+        dp[0][0] = 1;
+        
+        for(int i=1;i<=N;i++) 
+        {
+            for(int j=1;j<=sum/2;j++) 
+            {
+                if(nums[i-1] > j) 
+                    dp[i][j] = dp[i-1][j];
+                else 
+                    dp[i][j] = (dp[i-1][j] || dp[i-1][j-nums[i-1]]);
+            }
+        }
+        
+        return dp[N][sum/2];
+        
     }
 };
