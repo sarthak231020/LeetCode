8ec30bcb06abdfe69565952fe7c9dd5bab0185dd
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-01-06 12:41:14 +0530

    Time: 22 ms (56.96%), Space: 10.2 MB (37.13%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -23,20 +23,62 @@ public:
         return dp[i][j] =  min(up,min(upleft,upright)); 
     }
     
-    
-    
-    int minFallingPathSum(vector<vector<int>>& matrix) {
+    int solveTabulated(vector<vector<int>>& matrix,vector<vector<int>> &dp)
+    {
         int n = matrix.size();
         
         
-        int ans = INT_MAX;
-        
-        for(int i=0;i<n;i++) 
+        //Converting the base case.
+        for(int j=0;j<n;j++) 
         {
-            vector<vector<int>> dp(n+1,vector<int> (n+1,-1)); 
-            ans = min(ans,solve(n-1,i,matrix,dp));
+            dp[0][j] = matrix[0][j];
+        }
+        
+        for(int i=1;i<n;i++) 
+        {
+            for(int j=0;j<n;j++) 
+            {
+                int up = matrix[i][j] + dp[i-1][j]; 
+
+                int upleft = INT_MAX; 
+                if(j-1 >= 0)
+                {
+                    upleft = matrix[i][j] + dp[i-1][j-1]; 
+                }
+                int upright = INT_MAX;
+                if(j + 1 < matrix.size()) 
+                    upright = matrix[i][j] + dp[i-1][j+1]; 
+
+                dp[i][j] =  min(up,min(upleft,upright)); 
+            }
+        }
+        
+        int ans = INT_MAX; 
+        
+        for(int j=0;j<n;j++) 
+        {
+            ans = min(ans,dp[n-1][j]);
         }
         
         return ans;
+        
+    }
+    
+    
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        // Recursive Memoized Code 
+        int n = matrix.size();
+        // int ans = INT_MAX;
+        // for(int i=0;i<n;i++) 
+        // {
+        //     vector<vector<int>> dp(n+1,vector<int> (n+1,-1)); 
+        //     ans = min(ans,solve(n-1,i,matrix,dp));
+        // }
+        // return ans;
+        
+        // Tabulated Code 
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+        return solveTabulated(matrix,dp);
+        
     }
 };
