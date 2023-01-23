039c3aa59af728ce4d1b60404166387c39d9f89c
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-01-23 16:19:18 +0530

    Time: 296 ms (32.47%), Space: 60.7 MB (86.87%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        if(n == 1) 
-            return 1;
-        unordered_map<int,vector<int>> mp1;  // key is the person which trusted by vector<int> peoples.
-        unordered_map<int,int> mp2; // key,value => key (person who trust) the other person.
+        vector<int> count(n+1);
         
         for(int i=0;i<trust.size();i++) 
         {
-            mp1[trust[i][1]].push_back(trust[i][0]);
-        }
-        
-        for(int i=0;i<trust.size();i++) 
-        {
-            mp2[trust[i][0]] = trust[i][1];
+            count[trust[i][0]]--;
+            count[trust[i][1]]++;
         }
         
         for(int i=1;i<=n;i++) 
         {
-            if(mp1.find(i) != mp1.end() && mp1[i].size() == n-1 && mp2.find(i) == mp2.end())
+            if(count[i] == n-1) 
                 return i;
         }
+        
         return -1;
         
     }
