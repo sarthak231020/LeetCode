a097c80e9b71565d611959459a9e191635e2328d
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-02-14 19:29:12 +0530

    Time: 23 ms (84.40%), Space: 14.2 MB (52.80%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -13,20 +13,55 @@ public:
             }
         }
     }
+    
+    // we can also solve it using bfs.
+    void bfs(int node,vector<int> &visited,vector<vector<int>>& isConnected)
+    {
+        
+        queue<int> q;
+        q.push(node);
+        visited[node] = 1;
+        
+        while(!q.empty()) 
+        {
+            int top = q.front(); 
+            q.pop();
+            
+            for(int j=0;j<isConnected[top].size();j++) 
+            {
+                if(isConnected[top][j] == 1 && visited[j] == 0) 
+                {
+                    visited[j] = 1; //mark that node as visited. 
+                    q.push(j); // push that node to queue.
+                }
+            }
+        }
+        
+    }
+    
+    
     int findCircleNum(vector<vector<int>>& isConnected) {
         int N = isConnected.size(); // Number of nodes present.
         vector<int> visited(N+1,0); //Initializing the visited array as 0.
         
         int components = 0;
+        // for(int i=0;i<N;i++) 
+        // {
+        //     if(visited[i] == 0) 
+        //     {
+        //         components++;
+        //         dfs(i,visited,isConnected);
+        //     }
+        // }
+        
         for(int i=0;i<N;i++) 
         {
             if(visited[i] == 0) 
             {
                 components++;
-                dfs(i,visited,isConnected);
+                bfs(i,visited,isConnected);
             }
         }
-        
         return components;
         
     }
