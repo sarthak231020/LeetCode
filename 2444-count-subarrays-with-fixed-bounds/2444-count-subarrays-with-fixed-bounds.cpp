dc2c78aa3c302ff3cf203c12a89eeddfe8f13e9a
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-03-04 16:53:21 +0530

    Time: 116 ms (80.00%), Space: 70.4 MB (90.20%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
-    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        // minPosition, maxPosition: the MOST RECENT positions of minK and maxK.
-        // leftBound: the MOST RECENT value outside the range [minK, maxK].
-        long long answer = 0;
-        int minPosition = -1, maxPosition = -1, leftBound = -1;
+    // Brutforce Solution TLE.
+    long long brut(vector<int>& nums, int minK, int maxK)
+    {
+        long long res = 0;
         
-        // Iterate over nums, for each number at index i:
-        for (int i = 0; i < nums.size(); ++i) {
-            // If the number is outside the range [minK, maxK], update the most recent leftBound.
-            if (nums[i] < minK || nums[i] > maxK)
-                leftBound = i;
-            
-            // If the number is minK or maxK, update the most recent position.
-            if (nums[i] == minK) 
-                minPosition = i;
-            if (nums[i] == maxK)
-                maxPosition = i;
-
-            // The number of valid subarrays equals the number of elements between leftBound and 
-            // the smaller of the two most recent positions (minPosition and maxPosition).
-            answer += max(0, min(maxPosition, minPosition) - leftBound);
+        for(int i=0;i<nums.size();i++) 
+        {
+            int mini = INT_MAX,maxi = INT_MIN;
+            for(int j=i;j<nums.size();j++) 
+            {
+                mini = min(mini,nums[j]);
+                maxi = max(maxi,nums[j]);
+                if(mini == minK && maxi == maxK) 
+                    res++;
+            }
         }
-        return answer;
+        
+        return res;
+    }
+    
+    long long optimized(vector<int> &nums,int minK,int maxK) 
+    {
+        long jbad=-1,jmin=-1,jmax=-1,res=0;
+        
+        for(int i=0;i<nums.size();i++) 
+        {
+            if(nums[i] < minK || nums[i] > maxK) 
+                jbad = i;
+            if(nums[i] == minK) 
+                jmin = i;
+            if(nums[i] == maxK) 
+                jmax = i;
+            res += max(0L,min(jmin,jmax)-jbad);
+        }
+        return res;
+    }
+    
+    
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        // return brut(nums,minK,maxK);        // O(N^2) TLE...
+        return optimized(nums,minK,maxK);
     }
 };
