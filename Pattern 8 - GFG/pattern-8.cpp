24cb8543301ee7c18737c99427abfe49ae4fabea
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-09-10 21:16:47 +0530

    Added solution - LeetHub

--- Pattern 8 - GFG/pattern-8.cpp
+++ Pattern 8 - GFG/pattern-8.cpp
@@ -9,17 +9,22 @@ public:
 	
 	void printTriangle(int n) {
 	    // code here
+	    
 	    for(int i=n-1;i>=0;i--) 
-        {
-            for(int j=1;j<=n*2-1;j++) 
-            {
-                if(j>= n-i && j<= n+i)
-                    cout<<"*";
-                else if(j < n-i)
-                    cout<<" ";
-            }
-            cout<<endl;
-        }
+	    {
+	        for(int j=1;j<=n*2-1;j++) 
+	        {
+	            if(j >= n-i && j <= n+i)
+	            {
+	                cout<<"*";
+	            }
+	            else if(j< n-i)
+	            {
+	                cout<<" ";
+	            }
+	        }
+	        cout<<endl;
+	    }
 	}
 };
 
