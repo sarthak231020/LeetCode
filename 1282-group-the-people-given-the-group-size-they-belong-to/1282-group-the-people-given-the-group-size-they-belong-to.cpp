95049cff8264b6b870fe5c5750b62004925173e1
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-09-11 21:43:02 +0530

    Time: 16 ms (25.17%), Space: 14 MB (12.32%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        // vector<vector<int>> ans;
+        // map<int,vector<int>> mp;
+        // int ind = 0;
+        // Brut Solution.
+        //      for(auto i:groupSizes) 
+        //         {
+        //             mp[i].push_back(ind);
+        //             ind++;
+        //         }
+        
+        // for(auto i:mp) 
+        // {
+        //     int ptr = 0;
+        //     for(int j=0;j<i.second.size()/i.first;j++)
+        //     {
+        //         vector<int> temp;
+        //         for(int k=0;k<i.first;k++) 
+        //         {
+        //             temp.push_back(mp[i.first][ptr++]);
+        //         }
+        //         ans.push_back(temp);
+        //     }
+        // }
+        
+        // Optimized.
         vector<vector<int>> ans;
-        map<int,vector<int>> mp;
-        int ind = 0;
-        for(auto i:groupSizes) 
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<groupSizes.size();i++) 
         {
-            mp[i].push_back(ind);
-            ind++;
-        }
-        
-        
-//         for(auto i:mp) 
-//         {
-//             cout<<i.first<<endl;
-//             for(auto j:i.second) 
-//             {
-//                 cout<<j<<" ";
-//             }
-//             cout<<endl;
-//         }
-        
-        
-        
-        for(auto i:mp) 
-        {
-            int ptr = 0;
-            for(int k=0;k<i.second.size()/i.first;k++)
+            int key = groupSizes[i]; // the group size is the key.
+            
+            if(mp[key].size() < key) 
             {
-                vector<int> temp;
-                for(int j=0;j<i.first;j++) 
-                {
-                    temp.push_back(mp[i.first][ptr++]);
-                }
-                ans.push_back(temp);
-            } 
+                mp[key].push_back(i);
+            }
+            if(mp[key].size() == key)
+            {
+                ans.push_back(mp[key]);
+                mp.erase(key);
+            }
+            
         }
         
         return ans;
