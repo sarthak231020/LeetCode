db15501b12095c106740990e78811d90e3d48062
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2023-09-17 12:18:31 +0530

    Time: 22 ms (48.24%), Space: 22.8 MB (7.05%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+            
+        // Brut Solution 
+        
+        int N = arr.size();
+        if(N == 1) 
+            return arr[0];
+        if(arr[0] != arr[1]) 
+            return arr[0];
+        if(arr[N-1] != arr[N-2]) 
+            return arr[N-1];
+        
+        
+        for(int i=1;i<N-1;i++) 
+        {
+            if(arr[i] != arr[i-1] && arr[i] != arr[i+1]) 
+                return arr[i];
+        }
+        
+        return -1;
+        
+//         int N = arr.size();
+        
+//         if(N == 1) 
+//             return arr[0];
+        
+//         int low = 0, high = arr.size()-1;
+        
+//         if(arr[low] != arr[low+1])
+//             return arr[low];
+//         if(arr[high] != arr[high-1]) 
+//             return arr[high];
+        
+//         // int low = 0, high = N-1;
+        
+//         while(low <= high) 
+//         {
+//             int mid = (low + high)/2;
+            
+//             if(arr[mid-1] != arr[mid] && arr[mid+1] != arr[mid]) 
+//                 return arr[mid];
+            
+//             if(mid%2 == 0 && arr[mid-1] == arr[mid])
+//             {
+//                 high = mid-1;
+//             }
+//             else if(mid%2 == 1 && arr[mid-1] == arr[mid])
+//             {
+//                 low = mid+1;
+//             }
+//         }
+        
+//         return -1;
+    }
+};
