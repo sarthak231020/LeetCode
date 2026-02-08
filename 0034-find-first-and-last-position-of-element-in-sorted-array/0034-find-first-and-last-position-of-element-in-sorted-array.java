45584c75d38d0974d8018f2a4c57e665ca4066a0
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2026-02-08 14:42:55 +0530

    Time: 0 ms (100%), Space: 48.2 MB (47.1%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,49 +1,42 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
+
+    public int binarySearch(int[] nums,int target,boolean forFirst) 
+    {
         int n = nums.length;
         int low = 0, high = n-1;
+        int ans = -1;
+
+        while(low <= high) 
+        {
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target) 
+            {
+                ans = mid;
+                if(forFirst) 
+                    high = mid-1;
+                else
+                    low = mid+1;
+            }
+            else if(nums[mid] > target) 
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        
         int first = -1, last = -1;
 
-        while(low <= high) 
-        {
-            int mid = low + (high - low)/2;
-
-            if(nums[mid] == target) 
-            {
-                first = mid;
-                high = mid-1;
-            }
-            else if(nums[mid] > target) 
-            {
-                high = mid-1;
-            }
-            else
-            {
-                low = mid+1;
-            }
-        }
-
-        low = 0;
-        high = n-1;
-        while(low <= high) 
-        {
-            int mid = low + (high - low)/2;
-
-            if(nums[mid] == target) 
-            {
-                last = mid;
-                low = mid+1;
-            }
-            else if(nums[mid] > target) 
-            {
-                high = mid-1;
-            }
-            else
-            {
-                low = mid+1;
-            }
-        }
-
+        first = binarySearch(nums,target,true);
+        last = binarySearch(nums,target,false);
 
         return new int[] {first,last};
     }
