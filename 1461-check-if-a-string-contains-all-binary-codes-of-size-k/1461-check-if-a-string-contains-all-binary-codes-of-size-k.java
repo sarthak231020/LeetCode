839582cac47e5c26c3ba66b1b93ded2b4905d69e
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2026-03-22 17:13:28 +0530

    Time: 479 ms (5.02%), Space: 57.4 MB (74.39%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int convertToDecimal(String s)
+    {
+        int n = s.length();
+        int j = 0;
+        int ans = 0;
+        for(int i=n-1;i>=0;i--)
+        {
+            ans += (s.charAt(i)-'0')*(int)Math.pow(2,j++);
+        }
+        return ans;
+    }
+
+    public boolean hasAllCodes(String s, int k) {
+        int n = s.length();
+        if(k > n) 
+            return false;
+        int[] freq = new int[(int)Math.pow(2,k)];
+        int i = 0, j = 0;
+
+        while(j < n) 
+        {
+            if(j-i+1 == k) 
+            {
+                freq[convertToDecimal(s.substring(i,j+1))]++;
+                i++;
+            }
+            j++;
+        }
+        for(int ele:freq) 
+            if(ele == 0)
+                return false;
+        return true;
+    }
+}
