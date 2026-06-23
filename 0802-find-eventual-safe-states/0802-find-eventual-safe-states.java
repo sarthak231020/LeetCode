774c569826006668ed34518ef039c4c61a6e7174
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2026-06-23 22:50:48 +0530

    Time: 4 ms (99.78%), Space: 63.7 MB (24.53%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,18 +1,19 @@
 class Solution {
 
-    public boolean containsCycle(int currNode,boolean[] visited,boolean[] pathVisited,int[][] graph) 
+    public boolean containsCycle(int currNode,boolean[] visited,boolean[] pathVisited,boolean[] safe,int[][] graph) 
     {
         visited[currNode] = true; 
         pathVisited[currNode] = true;
-
+        safe[currNode] = false;
         for(int adjNode:graph[currNode]) 
         {
             if(!visited[adjNode]) 
-                if(containsCycle(adjNode,visited,pathVisited,graph)) 
+                if(containsCycle(adjNode,visited,pathVisited,safe,graph)) 
                     return true;
             if(pathVisited[adjNode]) 
                 return true;
         }
+        safe[currNode] = true; 
         pathVisited[currNode] = false;
         return false;
     }
@@ -21,19 +22,34 @@ class Solution {
         int V = graph.length;
         boolean[] visited = new boolean[V];
         boolean[] pathVisited = new boolean[V];
+        boolean[] safe = new boolean[V];
         List<Integer> ans = new ArrayList<>();
+
+        // Brutforce.
+        // for(int i=0;i<V;i++) 
+        // {
+        //     if(!visited[i])
+        //     {
+        //         if(!containsCycle(i,visited,pathVisited,graph))
+        //             ans.add(i);
+        //     }
+        //     Arrays.fill(visited,false);
+        //     Arrays.fill(pathVisited,false);
+        // }
+
+        // Optimal.
         for(int i=0;i<V;i++) 
         {
-            if(!visited[i])
-            {
-                if(!containsCycle(i,visited,pathVisited,graph))
-                    ans.add(i);
-            }
-            Arrays.fill(visited,false);
-            Arrays.fill(pathVisited,false);
+            if(!visited[i]) 
+                containsCycle(i,visited,pathVisited,safe,graph);
         }
 
-        Collections.sort(ans);
+        for(int i=0;i<V;i++) 
+        {
+            if(safe[i]) 
+                ans.add(i);
+        }
+
         return ans;
     }
 }
