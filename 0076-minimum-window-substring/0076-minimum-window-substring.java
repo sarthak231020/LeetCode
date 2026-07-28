a3fd94e3a301ba845ba2ba4ca9b295414ea4fc48
sarthak231020 <47363097+sarthak231020@users.noreply.github.com>
2026-07-28 21:45:49 +0530

    Time: 369 ms (5%), Space: 48.5 MB (5.13%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -16,19 +16,25 @@ class Solution {
         if(t.length() > s.length())
             return "";
         int i = 0, j = 0, minLen = Integer.MAX_VALUE, n = s.length();
+        int subCount = 0,mainCounter=0;
         int[] minWin = new int[2];
         Map<Character,Integer> strMap = new HashMap<>();
         Map<Character,Integer> subStrMap = new HashMap<>();
-        for(char ch:t.toCharArray()) 
+        for(char ch:t.toCharArray()){
+            if(subStrMap.getOrDefault(ch,0)==0)
+                subCount++;
             subStrMap.put(ch,subStrMap.getOrDefault(ch,0)+1);
-        
+        }
+        System.out.println(subCount);
         while(j < n)
         {
             char rc = s.charAt(j);
             strMap.put(rc,strMap.getOrDefault(rc,0)+1);
-            while(contains(strMap,subStrMap))
+            if(subStrMap.containsKey(rc) && strMap.get(rc).equals(subStrMap.get(rc)))
+                mainCounter++;
+            System.out.println(mainCounter);
+            while(subCount==mainCounter)
             {
-
                 if(j-i+1 < minLen) 
                 {
                     minLen = j-i+1; 
@@ -37,13 +43,15 @@ class Solution {
                 }
                 char lc = s.charAt(i);
                 strMap.put(lc,strMap.getOrDefault(lc,0)-1);
+                if( subStrMap.containsKey(lc)  && strMap.get(lc)<subStrMap.get(lc))
+                    mainCounter--;
                 if(strMap.get(lc) == 0) 
                     strMap.remove(lc);
                 i++;
             }
             j++;
         }
-
+        
         return minLen == Integer.MAX_VALUE ? "" : s.substring(minWin[0],minWin[1]+1);
     }
 }
